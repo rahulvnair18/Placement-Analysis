6976a895229da7cfaf61ccc0b88edb50ba5d6465
Mr-Leftiee <mr.leftie18@gmail.com>
2025-07-31 16:04:29 +0530

    first commit

---
+++ client/src/features/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-bold">Login Page</h1>
+    </div>
+  );
+};
+export default LoginPage;
---
+++ client/src/features/pages/SplashScreen.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+const SplashScreen = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      {/*this is a sample landing page*/}
+    </div>
+  );
+};
+export default SplashScreen;
